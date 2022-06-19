e7a42b77addd60a354ab291f4bc67b12b76a76b9
IdrisShittu <shittuidris45@yahoo.com>
2022-06-19 23:08:55 +0000

    Create ZigZag.java

---
+++ leetcode/ZigZag.java
@@ -0,0 +1,40 @@
+/*The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+*/
+
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] result = new StringBuilder[numRows]; 
+        Boolean status=true; int j=0;
+        for(int i=0; i<numRows; i++)result[i]= new StringBuilder();
+        
+        if(numRows==1)return s;
+        
+        for(int i=0; i<s.length(); i++){
+          result[j].append(s.charAt(i));
+          if(status==true)j++;
+          if(status==false)j--;
+          if(j==numRows){
+              status=false;
+              j=numRows-2;
+          }
+          else if(j==-1){
+              j=1;
+              status=true;
+          }
+        }
+          
+        StringBuilder output = new StringBuilder();
+        
+        for(int i=0; i<result.length; i++){
+            output.append(result[i]);
+        }
+        
+        return output.toString();
+         
+    }
+}
