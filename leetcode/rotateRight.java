80048fe00ec00114abefb61448660912bcc3d37d
IdrisShittu <shittuidris45@yahoo.com>
2022-07-07 22:57:02 +0000

    Update rotateRight.java

--- leetcode/rotateRight.java
+++ leetcode/rotateRight.java
@@ -1,4 +1,10 @@
 /**
+Given the head of a linked list, rotate the list to the right by k places.
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -10,15 +16,43 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        
         if (head == null || head.next == null) return head;
-    for (int i = 0; i < k; i++) {
-      ListNode temp = head;
-      while (temp.next.next != null) temp = temp.next;
-      ListNode end = temp.next;
-      temp.next = null;
-      end.next = head;
-      head = end;
-    }
-    return head;
+         
+        ListNode temp=head;
+        int size=1;
+        while(temp.next!=null){
+            temp=temp.next;
+            size++;
+        }
+        
+        if(k==size)return head;
+        
+        temp.next=head;
+        
+        k=k%size;
+        
+        while(size!=k+1){
+            head=head.next;
+            size--;
+        }
+        
+        temp=head.next;
+        head.next=null;
+        return temp;
+        
+        
+        /* O(k*n)
+        if (head == null || head.next == null) return head;
+        for (int i = 0; i < k; i++) {
+            ListNode temp = head;
+            while (temp.next.next != null) temp = temp.next;
+             ListNode end = temp.next;
+             temp.next = null;
+             end.next = head;
+             head = end;
+        }
+        return head;
+        */
     }
 }
