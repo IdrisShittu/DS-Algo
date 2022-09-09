09da3a18f5a3b61459b48200bf0e66ee989ee285
IdrisShittu <shittuidris45@yahoo.com>
2022-09-09 02:54:11 +0000

    Update setZereoes.java

--- leetcode/setZereoes.java
+++ leetcode/setZereoes.java
@@ -1,3 +1,9 @@
+/*Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+*/
+
+
 class Solution {
     public void setZeroes(int[][] matrix) {
         int row=matrix.length, col=matrix[0].length;
