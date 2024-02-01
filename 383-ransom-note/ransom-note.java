149c4b0863558202cf1a6988509a0b23d8539782
IdrisShittu <shittuidris45@yahoo.com>
2024-02-01 18:22:27 +0000

    Time: 3 ms (71.62%) | Memory: 44.6 MB (62.61%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] num= new int[26];
+        for(int i=0;i<ransomNote.length();i++)num[ransomNote.charAt(i)-'a']++;
+        for(int i=0;i<magazine.length();i++)num[magazine.charAt(i)-'a']--;
+        for(int i=0;i<num.length;i++)if(num[i]>0)return false;
+        return true;
+
+        
+    }
+}
