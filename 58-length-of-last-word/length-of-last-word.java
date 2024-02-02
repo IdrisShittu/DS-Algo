2ecb7efb8bc792a15797b298650a5786eab095f0
IdrisShittu <shittuidris45@yahoo.com>
2024-02-02 00:52:48 +0000

    Time: 0 ms (100.00%) | Memory: 41.6 MB (41.75%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str=s.split(" ");
-        return str[str.length-1].length();
+        int len=0; Boolean letterNotSeen=true;
+        for(int i=s.length()-1;i>=0;i--){
+            if(letterNotSeen && Character.isLetter(s.charAt(i)))letterNotSeen=false;
+            if(!letterNotSeen && Character.isLetter(s.charAt(i)))len++;
+            if(!letterNotSeen && !Character.isLetter(s.charAt(i)))break;  
+         }
+        return len;
     }
 }
