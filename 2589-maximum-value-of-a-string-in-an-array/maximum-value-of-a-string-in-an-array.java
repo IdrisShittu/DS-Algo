8d1fc7b108a2a99a99c3b0621fe3396b2918c96c
IdrisShittu <shittuidris45@yahoo.com>
2024-02-02 10:29:25 +0000

    Time: 16 ms (6.13%) | Memory: 42.1 MB (15.50%) - LeetSync

---
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumValue(String[] strs) {
+       int max=0;
+       boolean areAllDigits=true;
+       for(String s : strs){
+         for(int i=0;i<s.length();i++){
+             if(!Character.isDigit(s.charAt(i))){
+                 areAllDigits=false;
+                 break; 
+             }
+         }  
+         max = (areAllDigits)?Math.max(max,Integer.parseInt(s)):Math.max(max,s.length());
+         areAllDigits=true;
+         System.out.println(max+" "+areAllDigits);
+       } 
+       return max;
+    }
+}
