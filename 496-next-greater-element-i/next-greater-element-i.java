6a3ad65fd00561c5594a9f91221c6170955eacb1
IdrisShittu <shittuidris45@yahoo.com>
2024-02-04 21:56:30 +0000

    Time: 7 ms (11.98%) | Memory: 44.2 MB (24.21%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int j;
+        int val;
         HashMap<Integer,Integer> nums3 = new HashMap<>();
+        Stack<Integer> myStack =new Stack<>();
         for(int i=0;i<nums2.length;i++)nums3.put(nums2[i],i);
+        for(int i=nums2.length-1;i>=0;i--){
+            val=nums2[i];
+           
+            while(!myStack.isEmpty() && myStack.peek()<val)myStack.pop();
+            if(!myStack.isEmpty())nums2[i]=myStack.peek();
+            else nums2[i]=-1;
+            myStack.add(val);
+        }
+        System.out.println(Arrays.toString(nums2));
+        System.out.println(myStack.toString());
+
         for(int i=0;i<nums1.length;i++){
-            j=nums3.get(nums1[i]);
-            if(j==nums2.length-1)nums1[i]=-1;
-            for(int k=j+1; k<nums2.length;k++){
-                if(nums2[k]>nums2[j]){
-                    nums1[i]=nums2[k];
-                    break;
-                }
-                if(k==nums2.length-1)nums1[i]=-1;         
-            }
+            nums1[i]=nums2[nums3.get(nums1[i])];
         }
         return nums1;
     }
