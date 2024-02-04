e44d00f38290d24f851a8bf5166fafc41431259f
IdrisShittu <shittuidris45@yahoo.com>
2024-02-04 21:57:50 +0000

    Time: 4 ms (53.57%) | Memory: 44.4 MB (13.18%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -6,15 +6,11 @@ class Solution {
         for(int i=0;i<nums2.length;i++)nums3.put(nums2[i],i);
         for(int i=nums2.length-1;i>=0;i--){
             val=nums2[i];
-           
             while(!myStack.isEmpty() && myStack.peek()<val)myStack.pop();
             if(!myStack.isEmpty())nums2[i]=myStack.peek();
             else nums2[i]=-1;
             myStack.add(val);
         }
-        System.out.println(Arrays.toString(nums2));
-        System.out.println(myStack.toString());
-
         for(int i=0;i<nums1.length;i++){
             nums1[i]=nums2[nums3.get(nums1[i])];
         }
