08818d5384de50629b919b29e2de0a00b51e194e
IdrisShittu <shittuidris45@yahoo.com>
2024-02-05 04:51:52 +0000

    Time: 1 ms (80.38%) | Memory: 41.8 MB (56.90%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,14 +1,14 @@
 import java.util.regex.*;
 class Solution {
     public String truncateSentence(String s, int k) {
-        Pattern pattern = Pattern.compile(String.format("(\\b\\w+\\s){%d}(\\b\\w+)", k - 1));
-        Matcher matcher = pattern.matcher(s);
-     
-        if (matcher.find()) {
-            return matcher.group(0); 
-        } else {
-            return s; 
-        }
+
+       StringBuilder result = new StringBuilder();
+       String[] str=s.split(" ");
+       for(int i=0;i<k;i++){
+           result.append(str[i]);
+           if(i!=k-1)result.append(" ");
+       }
+       return result.toString();
     }
 }
 
