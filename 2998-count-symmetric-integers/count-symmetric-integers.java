2f50f221e5a964176deec4d2e339f4f88f1e1f6c
IdrisShittu <shittuidris45@yahoo.com>
2024-02-15 22:07:02 +0000

    Time: 115 ms (5.57%) | Memory: 41.4 MB (78.16%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.java
+++ 2998-count-symmetric-integers/count-symmetric-integers.java
@@ -1,38 +1,27 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        int count=0, sum, num,j;
-        Stack<Integer> myStack = new Stack<>();
-
+        int count=0;
         for(int i=low;i<=high;i++){
-            if(i<10)continue;
-            num=i;
-          //  System.out.println("Num-"+num);
-            while(num>0){
-                myStack.push(num%10);
-                num/=10;
-            }
-
-            if(myStack.size()%2!=0)continue;
-
-            num=myStack.size();
-            sum=0;
-            j=1;
-           // System.out.println("Stack-"+myStack);
-            while(j<=num/2){
-                //System.out.println("First-"+myStack.peek());
-                sum+=myStack.pop();
-                //System.out.println("Sum-"+sum);
-                j++;
-            }
-            while(j<=num){
-               // System.out.println("Second-"+myStack.peek());
-                sum-=myStack.pop();
-               // System.out.println("Sum-"+sum);
-                j++;
-            }
-
-            if(sum==0)count++;
+            if(isSymmetric(i))count++;
         }
         return count;
+    }
+
+    public boolean isSymmetric(int n){
+        int sum=0;
+        int len=getNumberOfDigits(n);
+        if(len%2!=0)return false;
+        for(int i=0;i<len/2;i++){
+            sum+=getNthDigit(n,i)-getNthDigit(n,len-i-1);
+        }
+        return sum==0;
+    }
+
+    public int getNumberOfDigits(int n){
+        return (int)Math.floor(Math.log10(n))+1;
+    }
+
+    public int getNthDigit(int n,int i){
+        return (int)Math.floor(n/(Math.pow(10,i))%10);
     }
 }
