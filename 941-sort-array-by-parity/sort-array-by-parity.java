bd83614d707fcd1dab636af2465184a5867d8537
IdrisShittu <shittuidris45@yahoo.com>
2024-02-17 00:27:11 +0000

    Time: 10 ms (6.74%) | Memory: 45.7 MB (5.17%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -2,14 +2,11 @@ import java.util.Arrays;
 import java.util.Comparator;
 class Solution {
     public static int[] sortArrayByParity(int[] nums) {
-        Integer[] boxedArray = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        
-        Arrays.sort(boxedArray, Comparator.comparingInt(a -> a % 2));
-        
+        Integer[] boxed = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(boxed, Comparator.comparingInt(a -> a % 2));
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = boxedArray[i];
+            nums[i] = boxed[i];
         }
-
         return nums;
     }
 }
