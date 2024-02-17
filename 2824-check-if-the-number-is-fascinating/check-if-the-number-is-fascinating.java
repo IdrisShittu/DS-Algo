0e2d9006a7f9e51ab06ada52443728a1415e4f7b
IdrisShittu <shittuidris45@yahoo.com>
2024-02-17 11:21:39 +0000

    Time: 18 ms (5.45%) | Memory: 41.4 MB (50.56%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
@@ -1,16 +1,28 @@
 class Solution {
     public boolean isFascinating(int n) {
         int[] freq = new int[10];
-        StringBuilder str = new StringBuilder(Integer.toString(n));
-        str.append(Integer.toString(2*n));
-        str.append(Integer.toString(3*n));
+        int num = 2*n;
 
-        String s= str.toString();
-        for(char ch : s.toCharArray()){
-            freq[ch-48]++;
+        while(num>0){
+            System.out.println("num1 "+num%10);
+            freq[num%10]++;
+            num/=10;
+        }
+
+        num = 3*n;
+        while(num>0){
+           // System.out.println("num2 "+num%10);
+            freq[num%10]++;
+            num/=10;
+        }
+
+        while(n>0){
+            freq[n%10]++;
+            n/=10;
         }
 
         for(int i=1;i<freq.length;i++){
+            System.out.println("freq " + freq[i]);
             if(freq[i]!=1)return false;
         }
 
