c22d3974470dfddb6e5d17e7c52f1dcce4110a62
IdrisShittu <shittuidris45@yahoo.com>
2024-02-20 23:36:40 +0000

    Time: 0 ms (100.00%) | Memory: 42.2 MB (49.41%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         for(int i=2; i<arr.length; i++){
-            if(arr[i]%2!=0){
-                if(arr[i]%2!=0 && arr[i-1]%2!=0 && arr[i-2]%2!=0)return true;
+            if((arr[i]&1)==1){
+                if((arr[i-1]&1)==1 && (arr[i-2]&1)==1)return true;
             }
         }
         return false;
