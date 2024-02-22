a0f51da97f762ec5cca653e51d9211a59ca1354c
IdrisShittu <shittuidris45@yahoo.com>
2024-02-22 07:22:39 +0000

    Time: 4 ms (14.04%) | Memory: 41.4 MB (80.80%) - LeetSync

---
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        Set<Integer> s = new HashSet();
+        for(int i=left; i<=right; i++){
+            s.add(i);
+        }
+
+        for(int[] range : ranges){
+            for(int i=range[0]; i<=range[range.length-1]; i++){
+                s.remove(i);
+            }
+        }
+
+        if(s.size()==0) return true;
+        return false;
+        
+    }
+}
