26249b169786b5acbe2a91e5510fe4d37dbb2997
IdrisShittu <shittuidris45@yahoo.com>
2024-03-01 23:44:05 +0000

    Time: 18 ms (45.36%) | Memory: 82.6 MB (20.62%) - LeetSync

--- 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.java
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.java
@@ -1,21 +1,26 @@
 class Solution {
     public long matrixSumQueries(int n, int[][] q) {
-        long totalSum = 0, cNo = 0, rNo = 0;
-        HashSet<Integer> r = new HashSet<>(), c = new HashSet<>();
-        for(int indx = q.length-1; indx > - 1 ; indx--){
-            var qs = q[indx];
-            if(qs[0] == 0 && !r.contains(qs[1])){
-                r.add(qs[1]);
-                rNo++;
-               totalSum += (qs[2] * (n-cNo));
-      
-            }else if(qs[0] == 1 && !c.contains(qs[1])){
-                c.add(qs[1]);
-                cNo++;
-               totalSum += (qs[2] * (n- rNo));
+        long totalSum = 0, rowCount = 0, colCount = 0;
+        HashSet<Integer> updatedRows = new HashSet<>();
+        HashSet<Integer> updatedCols = new HashSet<>();
+
+        for (int indx = q.length - 1; indx >= 0; indx--) {
+            var query = q[indx];
+            int operationType = query[0];
+            int index = query[1];
+            int value = query[2];
+
+            if (operationType == 0 && !updatedRows.contains(index)) {
+                updatedRows.add(index);
+                rowCount++;
+                totalSum += (value * (n - colCount));
+            } else if (operationType == 1 && !updatedCols.contains(index)) {
+                updatedCols.add(index);
+                colCount++;
+                totalSum += (value * (n - rowCount));
             }
-            
         }
+
         return totalSum;
     }
 }
