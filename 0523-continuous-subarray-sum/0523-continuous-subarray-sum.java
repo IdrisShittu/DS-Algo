7eb2110f5a9d8b6f6538348919ccd78a6722139f
IdrisShittu <shittuidris45@yahoo.com>
2024-06-08 22:29:26 -0700

    Time: 24 ms (44.20%), Space: 57.2 MB (83.85%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -8,7 +8,7 @@ class Solution {
             sum+=nums[i];
            
             if(mp.containsKey(sum%k)){
-                if(i-mp.get(sum%k)>1)return true;
+                 if(i-mp.get(sum%k)>1)return true;
             }
             else{ 
                 mp.put(sum%k,i);
