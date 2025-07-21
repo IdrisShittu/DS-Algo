7da69fefd000d606e3e1ffe446535cf0fa3f23bd
Idrishi <shittuidris45@yahoo.com>
2025-07-21 23:35:11 +0100

    Time: 26 ms (91.29%) | Memory: 46 MB (14.93%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -3,20 +3,24 @@ class Solution {
         int count=0;
         char prev='@';
         char str[]=s.toCharArray();
-        
+
+        StringBuilder res = new StringBuilder();
         for(int i=0; i<str.length; i++){
             if(str[i]==prev)count++;
             else count=1;
             prev=str[i];
-            if(count>=3)str[i]='@';
+           // if(count>=3)str[i]='@';
+           if(count>=3)continue;
+           res.append(str[i]);
         }
 
+      /*
         StringBuilder res = new StringBuilder();
         for(int i=0; i<str.length; i++){
             if(str[i]=='@')continue;
             res.append(str[i]);
         }
-
+*/
         return res.toString();
         
     }
